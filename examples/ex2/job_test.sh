feb84dd32a1e85ed45f5c57c657d758c5c1cc631
abylayzhumekenov <ablay94z@gmail.com>
2025-05-09 01:05:36 +0800

    test job

---
+++ examples/ex2/job_test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name=parsinv_ex2
+#SBATCH --output=job_%j.out
+#SBATCH --error=job_%j.err
+#SBATCH --mail-user=esmail.abdulfattah@kaust.edu.sa
+#SBATCH --mail-type=ALL
+#SBATCH --partition=workq
+#SBATCH --constraint=intel
+#SBATCH --nodes=1
+#SBATCH --ntasks=4
+#SBATCH --ntasks-per-node=4
+#SBATCH --ntasks-per-core=1
+#SBATCH --cpus-per-task=1
+#SBATCH --cpu-bind=threads
+#SBATCH --hint=nomultithread
+#SBATCH --mem-bind=v,local
+#SBATCH --mem-per-cpu=1G
+#SBATCH --time=00:10:00
+#SBATCH --report-bindings
+
+#load modules:
+module load mpi
+
+#OpenMP settings:
+export OMP_NUM_THREADS=1
+
+#run the application:
+srun ./main
