65915e1180fc3c1ef93de5c229b396fc85ac6e08
abylayzhumekenov <ablay94z@gmail.com>
2025-09-17 23:56:06 +0800

    update ex2/job.sh

--- examples/ex2/job.sh
+++ examples/ex2/job.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=parsinv_ex2
 #SBATCH --output=job_%j.out
 #SBATCH --error=job_%j.err
-#SBATCH --mail-user=esmail.abdulfattah@kaust.edu.sa
+#SBATCH --mail-user=djidenou.montcho@kaust.edu.sa
 #SBATCH --mail-type=ALL
 #SBATCH --partition=workq
 #SBATCH --constraint=intel
@@ -16,14 +16,17 @@
 #SBATCH --hint=nomultithread
 #SBATCH --mem-bind=v,local
 #SBATCH --mem-per-cpu=2G
-#SBATCH --time=10:00:00
+#SBATCH --time=15:00:00
 #SBATCH --report-bindings
 
 #load modules:
-module load mpi
+module load openmpi
 
 #OpenMP settings:
 export OMP_NUM_THREADS=1
+export PETSC_DIR=/home/montchd/test_abylay/petsc
+export PETSC_ARCH=arch-linux-c-debug
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PETSC_DIR/$PETSC_ARCH/lib
 
 #run the application:
 srun ./main -ni 100 -lr 1.0 -dr 0.8 -nr 10
